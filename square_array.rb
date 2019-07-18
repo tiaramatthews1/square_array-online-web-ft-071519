be63cc075133f65d80afe03cc21b6f0dd502390a
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-18 00:44:44 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+array.each 
+end
+
+def square_array(array)
+  numbers = []
+  array.each do |one_number|
+    numbers << one_number**2
+  end
+  numbers
 end
